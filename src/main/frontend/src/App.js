186ab9328970150be0623b30183b009c0b9b46d2
Anastasiia Gorelova <agorelova@Anastasiias-MacBook-Air.local>
2022-10-26 11:08:45 +0200

    frontend init

---
+++ src/main/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Appbar from './components/Appbar'
+import Appbarold from './components/Appbarold'
+
+function App() {
+  return (
+    <div className="App">
+      <Appbarold/>
+
+    </div>
+  );
+}
+
+export default App;
