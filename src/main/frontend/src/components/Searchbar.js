624046195e0f115b1c59f0f1db2d7b25f8038785
Anastasiia Gorelova <agorelova@Anastasiias-MacBook-Air.local>
2022-10-26 12:00:00 +0200

    search bar

--- src/main/frontend/src/components/Searchbar.js
+++ src/main/frontend/src/components/Searchbar.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import { Container, Paper } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
-      width: '100ch',
+      width: '90ch',
     },
   },
 }));
 
 export default function Searchbar() {
-  const classes = useStyles();
+    const paperStyle={padding:'50px 20px', width:1000, margin:"20px auto"}
+    const classes = useStyles();
 
   return (
+    <Container>
+        <Paper elevation={3} style={paperStyle}>
     <form className={classes.root} noValidate autoComplete="off">
       <TextField id="outlined-basic" label="Search words" variant="outlined" />
     </form>
+        </Paper>
+    </Container>
   );
 }
